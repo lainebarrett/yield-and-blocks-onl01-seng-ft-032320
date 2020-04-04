dd262f5f4cc576e722f682b6ac5ea97a0f3d0ac4
Laine Barrett <laine.barrett2019@gmail.com>
2020-04-04 02:15:12 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,10 @@
+def hello_t
 
+end
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
- if name.start_with?("T")
-   puts "Hi, #{name}"
- end
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
